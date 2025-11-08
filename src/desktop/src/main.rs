867672bd5e939e044409fe60f3b76be24e2a06ae
enekocm7 <enekocanedo@gmail.com>
2025-11-08 02:07:15 +0100

    Add logo for Desktop

--- src/desktop/src/main.rs
+++ src/desktop/src/main.rs
@@ -1,3 +1,4 @@
+use dioxus::desktop::tao::window::Icon;
 use dioxus::desktop::{Config, WindowBuilder};
 use dioxus::prelude::*;
 use ui::desktop::desktop_web_components::Desktop;
@@ -7,9 +8,11 @@ const MAIN_CSS: Asset = asset!("/assets/main.css");
 fn main() {
     LaunchBuilder::new()
         .with_cfg(
-            Config::default()
-                .with_menu(None)
-                .with_window(WindowBuilder::new().with_title("Nexu")),
+            Config::default().with_menu(None).with_window(
+                WindowBuilder::new()
+                    .with_title("Nexu")
+                    .with_window_icon(load_icon()),
+            ),
         )
         .launch(App);
 }
@@ -21,4 +24,13 @@ fn App() -> Element {
 
         Desktop {}
     }
+}
+
+fn load_icon() -> Option<Icon> {
+    let icon_bytes = include_bytes!("../assets/logo.png");
+    let img = image::load_from_memory(icon_bytes).expect("Failed to load icon image");
+
+    let rgba = img.to_rgba8();
+    let (width, height) = rgba.dimensions();
+    Icon::from_rgba(rgba.into_raw(), width, height).ok()
 }
