0f603680a86c09bf5149ff7b9f85259286d2ecce
enekocm7 <enekocanedo@gmail.com>
2026-01-21 21:25:24 +0100

    Pass controller::AppController::new directly to use_signal

--- src/desktop/src/main.rs
+++ src/desktop/src/main.rs
@@ -35,7 +35,7 @@ fn main() {
 #[component]
 fn App() -> Element {
     let app_state = use_signal(|| AppState::new("Error"));
-    let controller = use_signal(|| controller::AppController::new());
+    let controller = use_signal(controller::AppController::new);
     let mut progress_bar = use_signal::<u64>(|| u64::MAX);
 
     use_effect(move || {
